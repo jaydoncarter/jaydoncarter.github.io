298c040a11dd5b8a56c1755485bd3168956196a2
Jaydon Carter <jaydoncarter898@gmail.com>
2025-06-21 17:14:48 -0600

    Added favicon, fixed typos, added footer, improved header, changed formatting.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
+// Interactive Navigation
 const navLinks = document.querySelectorAll('.nav a');
 navLinks.forEach(link => {
   if (link.href === window.location.href) {
     link.classList.add('active');
   }
 });
+
+// Update Copyright Year
+document.getElementById('year').textContent = new Date().getFullYear();
