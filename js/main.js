dcecd926209e4384c172a22460591ebdda20bd57
Jaydon Carter <jaydoncarter898@gmail.com>
2025-06-21 23:28:36 -0600

    Fixed navigation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 // Interactive Navigation
+const currentPath = window.location.pathname;
+
 const navLinks = document.querySelectorAll('.nav a');
 navLinks.forEach(link => {
-  if (link.href === window.location.href) {
+  const linkPath = new URL(link.href).pathname;
+
+  if (
+    (linkPath === '/index.html' && currentPath === '/') ||
+    linkPath === currentPath
+  ) {
     link.classList.add('active');
   }
 });
+
 
 // Update Copyright Year
 document.getElementById('year').textContent = new Date().getFullYear();
