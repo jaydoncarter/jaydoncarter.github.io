ffbbff005784fbd589334dd2d2e5af2c9ec88551
Jaydon Carter <jaydoncarter898@gmail.com>
2025-08-24 01:09:39 -0600

    Improved Home page.

--- js/main.js
+++ js/main.js
@@ -35,3 +35,23 @@ checkbox.addEventListener('change', () => {
     localStorage.setItem('theme', 'dark');
   }
 });
+
+// Image Slideshow
+const images = [
+      {src: "assets/meadow.jpg", title: "Apline meadow on the Forsyth backpacking trail in Pine Valley wilderness area." },
+      {src: "assets/desert-morning.jpg", title: "A beautiful sunrise on Church Rocks MTB trail near St. George UT." },
+      {src: "assets/hills.jpg", title: "Facing south-east from the top of the Breaks trail near Cedar City UT." },
+      {src: "assets/snow-forest.jpg", title: "Taken while nordic skiing in Deer Hollow in Dixie National Forest." }
+    ];
+
+const imgElement = document.getElementById("randomImage");
+const titleElement = document.getElementById("imageTitle");
+
+function changeImage() {
+  const randomIndex = Math.floor(Math.random() * images.length);
+  imgElement.src = images[randomIndex].src;
+  titleElement.textContent = images[randomIndex].title;
+}
+
+setInterval(changeImage, 5000);
+changeImage();
